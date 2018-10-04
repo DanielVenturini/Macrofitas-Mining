c0f43d5ab196cb538e26cb5bcc9360d069ad3409
Daniel Venturini <danielventurini021@gmail.com>
2018-10-04 13:49:47 -0300

    Leitura pronta. #2
    Já está lendo do arquivo de entrada e recuperando cada linha do arquivo

--- OperacoesArquivo.py
+++ OperacoesArquivo.py
@@ -1,45 +1,51 @@
 # coding:UTF-8
 
 '''
-Esta classe contera as implementaçoes das funçoes responsaveis por ler o arquivo
+Esta classe contém as implementações das funções responsáveis por ler o árquivo
 com os nomes das macrofitas e retornar nome a nome
+'''
+
+'''
+Use:
+    from OperacoesArquivo import (Reader, Writer)
+    leitor = Reader('ListaMacrofitas.xlsx')     # inicializa o leitor e abre o arquivo
+    leitor.getNome()                            # recupera um nome por linha já no padrão GET: 'genero%20especie'
 '''
 
 import openpyxl
 
 class Reader:
 
-    def __init__(self):
-        pass
-
-    def getLeitorEscritor(self, nomeArquivo):
+    def __init__(self, nomeArquivo):
         try:
-            book = openpyxl.load_workbook(nomeArquivo)
-            self.leitor = book.active
+            book = openpyxl.load_workbook(nomeArquivo)      # abrindo o arquivo para o leitor
+            self.leitor = book.active                       # ativando o leitor
         except FileNotFoundError:
             self.leitor = None
-            print("ARQUIVO {0} NAO EXISTE.".format(nomeArquivo))
             raise
 
-        self.linha = 1
-        return self, None#, Writer(nomeArquivo)
-
+        self.linha = 1          # começa em 1 porque na posição 0 é outra coisa
 
     # Esta funçao retorna nome a nome.
-    # Estes nomes ja foram previamente lidos do arquivo
+    # O valor retornado é 'genero%20especie', pois está no padrão para enviar o GET
+    # Gera a exceção AttributeError quando não houver mais linhas
     def getNome(self):
-        if not self.leitor:
-            raise Exception
+        if not self.leitor:     # se o leitor for inválido
+            raise Exception     # lança uma exceção
 
-        linha = self.leitor['A{0}'.format(str(self.linha))].internal_value.split(' ')
-        self.linha += 1
-        return linha[0] + '%20' + linha[1]        # Primeira coluna e o genero, segunda o nome da especie.
+        try:
+            linha = self.leitor['A{0}'.format(str(self.linha))].internal_value.split(' ')   # recupera o objeto da linha e desta, os valores da linha
+            self.linha += 1                     # atualiza o valor da linha
+            return linha[0] + '%20' + linha[1]  # Primeira coluna é o genero, segunda é o nome da espécie.
+        except AttributeError:                  # quando chegar no fim do arquivo
+            self.leitor = None                  # atribui None ao leitor, para, se chamar novamente, gere a exceção no bloco if
+            raise                               # Re-lança a exceção
 
 
 '''
-Esta classe contera as implementaçoes das funçoes responsaveis por escrever o arquivo.
-O que sera escrito sera o nome da planta, se e sinonimo, o nome alterado e a coordenada.
-Na primeira etapa, nao sera usado o campo coordenada.
+Esta classe contera as implementaçoes das funçoes responsaveis por escrever no arquivo.
+O que sera escrito sera o nome da planta, se é sinonimo, o nome alterado e a coordenada.
+Na primeira etapa, não será usado o campo coordenada.
 '''
 
 class Writer:
@@ -52,6 +58,3 @@ class Writer:
 
     def fim(self):
         self.file.close()   # fecha o arquivo e salva o conteudo
-
-
-leitor = Reader().getLeitorEscritor('ListaMacrofitas.xlsx')
