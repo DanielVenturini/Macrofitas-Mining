4604a8d4b5386f5a5cdf4d89d8a978d2234c1512
Luiz Silveira <luiz_ssilveira@hotmail.com>
2018-10-26 15:57:22 -0300

    testes arquivos Flora .py e Plantlist #18
    2

--- test/testPlantlist.py
+++ test/testPlantlist.py
@@ -3,5 +3,5 @@ from codicoMacrofitas.plantlist import urlPL
 
 class TestPlantlist(unittest.TestCase):
     
-    def test_urlPL(self):
+    def testUrlPL(self):
         self.assertEqual(urlPL('Dicliptera', 'ciliaris'), 'http://www.theplantlist.org/tpl1.1/search?q=Dicliptera+ciliaris')
