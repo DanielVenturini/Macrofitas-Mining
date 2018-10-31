dbbfc215383f91ba4947fbfda96937e939208308
Douglas Vinicius de Abreu <douglasabrel97@gmail.com>
2018-10-31 19:43:26 -0300

    Arrumando erro de conexão

--- codicoMacrofitas/gbif.py
+++ codicoMacrofitas/gbif.py
@@ -3,10 +3,14 @@ import requests
 def urlGB(nomePlanta,offset):
     return "http://api.gbif.org/v1/occurrence/search?limit=1&offset="+str(offset)+"&continent=SOUTH_AMERICA"
     +"scientificName=" + nomePlanta.replace(' ','%20')
+   
 
 def requisicaoGB(url):
-    return requests.get(url).json()
-
+    try:
+        return requests.get(url).json()
+    except Exception as ex:
+        print("Err: " + str(ex))
+        return requisicaoGB(url)
 
 def dadosGB(jsonResp):
     try:
