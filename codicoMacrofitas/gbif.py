49a94b214c33fc983925c49adc0fdb49f9acc833
Douglas Vinicius de Abreu <douglasabrel97@gmail.com>
2018-10-31 19:28:23 -0300

    Iniciando as requisições GBIF

---
+++ codicoMacrofitas/gbif.py
@@ -0,0 +1,21 @@
+import requests
+
+def urlGB(nomePlanta,offset):
+    return "http://api.gbif.org/v1/occurrence/search?limit=1&offset="+str(offset)+"&continent=SOUTH_AMERICA"
+    +"scientificName=" + nomePlanta.replace(' ','%20')
+
+def requisicaoGB(url):
+    return requests.get(url).json()
+
+
+def dadosGB(jsonResp):
+    try:
+        if(jsonResp['results']):
+            for result in jsonResp['results']:
+                print(result['stateProvince'])
+
+    except Exception as ex:
+        print("Erro")
+url = urlGB("justicia pectoralis",10)
+
+dadosGB(requisicaoGB(url))
