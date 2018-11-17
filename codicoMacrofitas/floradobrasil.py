c692c77e85c0c1e639d19052d273d8305b958d67
Luiz Silveira <luiz_ssilveira@hotmail.com>
2018-11-16 23:16:58 -0200

    função para pegar informação Flora do Brasil
    3h

--- codicoMacrofitas/floradobrasil.py
+++ codicoMacrofitas/floradobrasil.py
@@ -1,7 +1,8 @@
 import requests
+from bs4 import BeautifulSoup
 
 def urlFB(nomePlanta):
-        return "http://servicos.jbrj.gov.br/flora/taxon/" + nomePlanta.replace(' ','%20')
+    return "http://servicos.jbrj.gov.br/flora/taxon/" + nomePlanta.replace(' ','%20')
 
 def requisicaoFB(url):
     return requests.get(url).json()
@@ -47,6 +48,45 @@ def getSinonimosFlora(nomePlanta, jsonResp):
     except Exception as ex:
         print("Erro: {0} -- {1}".format(nomePlanta, ex))
         return []
+
+
+from floraInfo import FloraInfo
+
+def getURLID(nomePlanta):
+    resp = requisicaoFB('http://servicos.jbrj.gov.br/flora/url/' + nomePlanta.replace(' ', '%20'))
+    return 'http://reflora.jbrj.gov.br/reflora/listaBrasil/ConsultaPublicaUC/ResultadoDaConsultaCarregaTaxonGrupo.do?&idDadosListaBrasil=' + resp["result"][0]["references"].split('=FB')[1]
+
+def getInfoFlora(nomePlanta, info, jsonResp):
+    siteFlora = jsonResp
     
-# nomePlanta = 'Sesuvium portulacastrum'
-# getSinonimosFlora(nomePlanta, requisicaoFB(urlFB(nomePlanta)))
+    soup = BeautifulSoup(siteFlora['hierarquia'], "html.parser")
+    info.familia = soup.find("div", class_="taxon").text + soup.find("div", class_="nomeAutorSupraGenerico").text
+    info.taxonomica = soup.findAll("div", class_="grupo")[1].text
+    
+    info.substrato = siteFlora['substrato']
+    info.formaVida = siteFlora['formaVida']
+    info.endemismo = siteFlora['endemismo']
+    info.origem = siteFlora['origem']
+    info.vegetacao = siteFlora['tipoVegetacao']
+    info.fitogeografico = siteFlora['dominioFitogeografico']
+
+    DGC = ['distribuicaoGeografica-Nordeste', 'distribuicaoGeografica-CentroOeste', 'distribuicaoGeografica-Norte', 'distribuicaoGeografica-Sul', 'distribuicaoGeografica-Sudeste']
+   
+    for local in DGC:
+        certeza = local.replace('-', 'Certeza')
+        if(siteFlora[certeza]):
+            pos1 = siteFlora[certeza].index('(') + 1
+            pos2 = siteFlora[certeza].index(')')
+            info.ocorenciaConfirmada = (siteFlora[certeza][pos1:pos2].split(', '))
+        
+        duvida = local.replace('-', 'Duvida')
+        if(siteFlora[duvida]):
+            pos1 = siteFlora[duvida].index('(') + 1
+            pos2 = siteFlora[duvida].index(')')
+            info.possiveisOcorrencias = (siteFlora[duvida][pos1:pos2].split(', '))
+    info.printInfo()
+
+
+# nomePlanta = 'Microlicia fasciculata'
+# fInfo = FloraInfo(nomePlanta)
+# getInfoFlora(nomePlanta, fInfo, requisicaoFB(getURLID(nomePlanta)))
